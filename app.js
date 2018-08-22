647e97ced97add8599cbefad5e9271a394b2e140
Mohammad Abdullah Saed <mohdsaed@hungerstation.com>
2018-08-22 20:22:08 +0300

    Fetch data from graphql

--- app.js
+++ app.js
@@ -4,14 +4,69 @@ const { buildSchema } = require('graphql');
 
 const app = express();
 
+const coursesData = [
+  {
+    id: 1,
+    title: 'The Complete Node.js Developer Course',
+    author: 'Andrew Mead, Rob Percival',
+    description: 'Learn Node.js by building real-world applications with Node, Express, MongoDB, Mocha, and more!',
+    topic: 'Node.js',
+    url: 'https://codingthesmartway.com/courses/nodejs/'
+  },
+  {
+    id: 2,
+    title: 'Node.js, Express & MongoDB Dev to Deployment',
+    author: 'Brad Traversy',
+    description: 'Learn by example building & deploying real-world Node.js applications from absolute scratch',
+    topic: 'Node.js',
+    url: 'https://codingthesmartway.com/courses/nodejs-express-mongodb/'
+  },
+  {
+    id: 3,
+    title: 'JavaScript: Understanding The Weird Parts',
+    author: 'Anthony Alicea',
+    description: 'An advanced JavaScript course for everyone! Scope, closures, prototypes, this, build your own framework, and more.',
+    topic: 'JavaScript',
+    url: 'https://codingthesmartway.com/courses/understand-javascript/'
+  }
+];
+
 const schema = buildSchema(`
   type Query {
     message: String
+    course(id: Int!): Course
+    courses(topic: String): [Course]
+  }
+
+  type Course {
+    id: Int
+    title: String
+    author: String
+    description: String
+    topic: String
+    url: String
   }
 `);
 
+const getCourse = args => {
+  const id = args.id;
+  return coursesData.filter(course => course.id === id)[0];
+};
+
+const getCourses = args => {
+  let topic;
+  if (args.topic) {
+    topic = args.topic;
+    return coursesData.filter(course => course.topic === topic);
+  } else {
+    return coursesData;
+  }
+};
+
 const root = {
-  message: () => 'Hello, World!'
+  message: () => 'Hello, World!',
+  course: getCourse,
+  courses: getCourses
 };
 
 app.use('/graphql', expressGraphql({
